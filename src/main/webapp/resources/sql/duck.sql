12cb3a8fd7d01bcedbdc914a78f22f59c653583e
slhague <susan@adsc.com>
2016-04-27 10:30:00 -0400

    Updated duck.sql to include all updates

--- src/main/webapp/resources/sql/duck.sql
+++ src/main/webapp/resources/sql/duck.sql
@@ -1,6 +1,4 @@
 create database DuckDb;
-
-grant all on DuckDb.* to 'admin'@'localhost' identified by 'susan';  
  
 CREATE TABLE DuckDb.`duck_user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,6 +14,7 @@ CREATE TABLE DuckDb.`duck_user` (
   `password` varchar(45) NOT NULL,
   `active` boolean NOT NULL default 0,
   `registration_date` DATE NOT NULL,
+  `profile_image` varchar(255) ,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -80,4 +79,54 @@ CREATE TABLE DuckDb.`review_issue` (
   `issue_title` varchar(45) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`issue_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE DuckDb.`opportunity_time` (
+  `opportunity_time_id` int(11) NOT NULL AUTO_INCREMENT,
+  `opportunity_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `work_date` DATE NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  PRIMARY KEY (`opportunity_time_id`),
+  INDEX(`user_id`),
+  INDEX(`opportunity_id`),
+  FOREIGN KEY (`user_id`) REFERENCES DuckDb.`duck_user`(`user_id`),
+  FOREIGN KEY (`opportunity_id`) REFERENCES DuckDb.`opportunity`(`opportunity_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE DuckDb.`mail_message` (
+  `mail_message_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `sent` DATETIME NOT NULL,
+  `send_to` TEXT NOT NULL,
+  `subject` varchar(45),
+  `body` LONGTEXT NOT NULL,
+  PRIMARY KEY (`mail_message_id`),
+  INDEX(`sent_id`),
+  FOREIGN KEY (`sent_id`) REFERENCES DuckDb.`duck_user`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE DuckDb.`mail_distribution` (
+  `mail_distribution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail_message_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`mail_distribution_id`),
+  INDEX(`mail_message_id`),
+  INDEX(`user_id`),
+  FOREIGN KEY (`mail_message_id`) REFERENCES DuckDb.`mail_message`(`mail_message_id`),
+  FOREIGN KEY (`user_id`) REFERENCES DuckDb.`duck_user`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE DuckDb.`mail_box` (
+  `mail_box_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail_message_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `folder` varchar(45),
+  `is_read` boolean NOT NULL default 0, 
+  PRIMARY KEY (`mail_box_id`),
+  INDEX(`mail_message_id`),
+  INDEX(`user_id`),
+  FOREIGN KEY (`mail_message_id`) REFERENCES DuckDb.`mail_message`(`mail_message_id`),
+  FOREIGN KEY (`user_id`) REFERENCES DuckDb.`duck_user`(`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
